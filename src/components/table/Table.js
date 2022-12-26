53363a766359090c0a46159048ed4a95a7e565f1
sergij14 <sergi.jajanidze@gmail.com>
2022-12-26 21:12:21 +0400

    fixed markup: added border-right

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -25,7 +25,7 @@ export class Table extends ExcelComponent {
       </div>
     </div>
     <div class="flex border-b-2 border-gray-300">
-      <div class="flex w-[80px] border-gray-200 bg-gray-100">1</div>
+      <div class="flex w-[80px] border-r-2 border-gray-200 bg-gray-100">1</div>
       <div class="flex">
         <div
           class="border-r-2 border-gray-200 w-[180px] flex justify-center"
