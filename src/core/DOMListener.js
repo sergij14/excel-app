4bc4b43414916a39aa22698141707f66354d8a02
sergij14 <sergi.jajanidze@gmail.com>
2022-12-27 18:41:37 +0400

    adding event listeners on Components

--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,16 +1,30 @@
+import {capitalize} from './utils';
+
 export class DOMListener {
   constructor($root, listeners = []) {
     if (!$root) {
-      return;
+      throw new Error('No $root was provided for DOMListener');
     }
     this.$root = $root;
     this.listeners = listeners;
   }
 
   initDOMListeners() {
-    console.log(this.listeners);
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        const name = this.name;
+        throw new Error(`${method} is not implemented for ${name} Component`);
+      }
+      this.$root.addEventListener(listener, this[method].bind(this));
+    });
   }
 
   removeDOMListeners() {
   }
+}
+
+
+function getMethodName(evtName) {
+  return 'on' + capitalize(evtName);
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,6 +3,7 @@ import {DOMListener} from './DOMListener';
 export class ExcelComponent extends DOMListener {
   constructor($root, config = {}) {
     super($root, config.listeners);
+    this.name = config.name;
   }
   toHTML() {
     return '';
