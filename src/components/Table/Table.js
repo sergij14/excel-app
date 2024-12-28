dc266d816556c0ad2f688138a1f97d87f779e921
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2024-12-28 11:59:08 +0400

    feat: optimized rendering

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,6 +2,9 @@ import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
 import { $ } from "../../core/dom";
 
+const MIN_CELL_WIDTH = 40;
+const MIN_CELL_HEIGHT = 20;
+
 export class Table extends ExcelComponent {
   static cn = "excel-table";
 
@@ -22,25 +25,42 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"');
       const parentCoords = $parent.getCoords();
       const resizeType = $resizer.dataset.resize;
-      const $cells = this.$el.findAll(`[data-col="${$parent.dataset.col}"]`);
+      const sideProp = resizeType === "col" ? "bottom" : "right";
+
+      let width, height;
+      $resizer.css({ opacity: 1, [sideProp]: "-5000px" });
 
       document.onmousemove = (moveEv) => {
         if (resizeType === "col") {
           const delta = moveEv.pageX - parentCoords.right;
-          const newWidth = `${parentCoords.width + delta}px`;
-
-          $parent.css({ width: newWidth });
-          $cells.forEach((el) => $(el).css({ width: newWidth }));
+          width = parentCoords.width + delta;
+          $resizer.css({ right: `${-delta}px` });
         } else {
           const delta = moveEv.pageY - parentCoords.bottom;
-          const newHeight = `${parentCoords.height + delta}px`;
-
-          $parent.css({ height: newHeight });
+          height = parentCoords.height + delta;
+          $resizer.css({ bottom: `${-delta}px` });
         }
       };
 
       document.onmouseup = () => {
         document.onmousemove = null;
+        document.onmouseup = null;
+
+        $resizer.css({ opacity: 0, bottom: 0, right: 0 });
+
+        if (resizeType === "col") {
+          if (width < MIN_CELL_WIDTH) return;
+
+          const $cells = this.$el.findAll(
+            `[data-col="${$parent.dataset.col}"]`
+          );
+          $parent.css({ width: `${width}px` });
+          $cells.forEach((el) => $(el).css({ width: `${width}px` }));
+        } else {
+          if (height < MIN_CELL_HEIGHT) return;
+
+          $parent.css({ height: `${height}px` });
+        }
       };
     }
   }
