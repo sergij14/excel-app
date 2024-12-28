86544d28022f57162e32c4b0066109576dc2c9d0
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2024-12-28 21:09:59 +0400

    feat: created event emitter class

---
+++ src/core/Emitter.js
@@ -0,0 +1,34 @@
+export class Emitter {
+  constructor() {
+    this.listeners = {};
+    this.key = 0;
+  }
+
+  subscribe(eventName, cb) {
+    if (!Object.hasOwn(this.listeners, eventName)) {
+      this.listeners[eventName] = {};
+    }
+
+    const listenerId = this.key;
+    this.listeners[eventName][listenerId] = cb;
+    this.key++;
+
+    return () => {
+      delete this.listeners[eventName][listenerId];
+    };
+  }
+
+  emit(eventName, ...args) {
+    if (!Object.hasOwn(this.listeners, eventName)) {
+      return false;
+    }
+
+    const listeners = { ...this.listeners[eventName] };
+
+    Object.values(listeners).forEach((listener) => {
+      listener.apply(null, args);
+    });
+
+    return true;
+  }
+}
