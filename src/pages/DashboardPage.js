82d58eea6df4c0b0a78d603e66a170d1f1294737
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2024-12-31 11:30:14 +0400

    feat: created dashboard page markup

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,11 +1,58 @@
 import { Page } from "../core/Router/Page";
 import { $ } from "../core/DOM/dom";
 
-export class DashboardPage extends Page {
+function createRecord(record) {
+  return `
+        <li class="dashboard-list-item">
+          <a href="#">Table 2</a>
+          <span>12.06.2020</span>
+        </li>
+  `;
+}
 
+function createRecordsTable() {
+  const keys = getAllKeys();
+  console.log(keys);
+  
+
+  return `
+    <div class="dashboard-list">
+      <div class="dashboard-list-header">
+        <span>Table Name</span>
+        <span>Creation Date</span>
+      </div>
+      <ul>
+        ${keys.map(createRecord).join("")}
+      </ul>
+    </div>
+  `;
+}
+
+function getAllKeys() {
+  const keys = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (!key.includes("excel")) {
+      continue;
+    }
+    keys.push(key);
+  }
+
+  return keys;
+}
+
+export class DashboardPage extends Page {
   getContainer() {
-    const $container = $.create("div");
-    $container.text("dashboard");
+    const now = new Date().getTime();
+    const $container = $.create("div", "dashboard").html(`
+        <h1 class="dashboard-title">Excel App</h1>
+        <div class="dashboard-header">
+          <div class="dashboard-header-content">
+            <a href="#excel/${now}" class="dashboard-header-item">New Table</a>
+          </div>
+        </div>
+        ${createRecordsTable()}
+      `);
 
     return $container;
   }
